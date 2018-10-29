432cd812f2e7a3f61afe05696be40a89e1254e0b
Sepehr Parirokh <sepehrparirokh@gmail.com>
2018-10-29 12:28:51 -0400

    UI setup

--- server.js
+++ server.js
@@ -16,7 +16,11 @@ app.get("/", function(req, res) {
 io.sockets.on("connection", function(socket) {
   connections.push(socket);
   console.log("connceted %s sockets conncted", connections.length);
-
+  // Disconect
   connections.splice(connections.indexOf(socket), 1);
   console.log("disconnected %s socket connceted", connections.length);
+
+  // Disconect
+  connections.splice(connections.indexOf(socket), 1);
+  console.log("Disconnected: %s sockets connected", connections.length);
 });
